499036a40b653f3240f2b22eb16e152ffb0439bd
Kate Sanders <SandersKM@Hendrix.edu>
2019-01-30 10:34:45 -0600

    added Recylerview object dependencies

--- app/src/main/java/kate/workoutplanner/MainActivity.java
+++ app/src/main/java/kate/workoutplanner/MainActivity.java
@@ -1,22 +1,44 @@
 package kate.workoutplanner;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+    private Context mContext;
+    private RecyclerView todays_workout_list;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        // Get the application context
+        mContext = getApplicationContext();
+        // RecyclerView Objects
+        todays_workout_list = (RecyclerView) findViewById(R.id.todays_workout_list);
+        todays_workout_list.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        todays_workout_list.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        //mAdapter = new MyAdapter(myDataset);
+        todays_workout_list.setAdapter(mAdapter);
 
 
     }
